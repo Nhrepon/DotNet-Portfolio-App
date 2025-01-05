c24dcf1adefa1a6d1127031b6b6fe63db725ab27
nhrepon <nurhossainrepon7248@gmail.com>
2025-01-05 10:15:23 +0600

    config

---
+++ Portfolio/Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(255)]
+        public string Address { get; set; }
+
+        [StringLength(20)]
+        public string Phone { get; set; }
+
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        // Navigation property to related Equipment entities
+        public ICollection<Equipment> Equipments { get; set; }
+    }
+}
---
+++ Portfolio/Models/Equipment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.Models
+{
+    public class Equipment
+    {
+        [Key]
+        public int EquipmentId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        // Foreign key to the Customer entity
+        public int CustomerId { get; set; }
+
+        // Navigation property to the related Customer entity
+        public Customer Customer { get; set; }
+    }
+}
